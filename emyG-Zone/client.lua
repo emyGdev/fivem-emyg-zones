da5b9619a341ef4273c1d7a7ecc0eb6c0ec98211
emyg <169094775+emyGdev@users.noreply.github.com>
2025-03-01 23:06:11 +0300

    Update client.lua

--- emyG-Zone/client.lua
+++ emyG-Zone/client.lua
@@ -1,27 +1,11 @@
 local redZones = {
     {
-        x = 2349.61,    -- Red zone x koordinatı
-        y = 2557.08,    -- Red zone y koordinatı
-        z = 46.67,    -- Red zone z koordinatı
-        radius = 92.0,   -- Red zone yarıçapı
-        visibleDistance = 300.0, -- Marker'ın görüleceği maksimum mesafe
-        color = {r = 255, g = 0, b = 0, a = 128} -- Red zone renk (RGBA)
-    },
-    {
-        x = 1475.28,    -- Red zone x koordinatı
-        y = 6336.26,    -- Red zone y koordinatı
-        z = 18.66,    -- Red zone z koordinatı
-        radius = 92.0,   -- Red zone yarıçapı
-        visibleDistance = 300.0, -- Marker'ın görüleceği maksimum mesafe
-        color = {r = 255, g = 0, b = 0, a = 128} -- Red zone renk (RGBA)
-    },
-    {
-        x = 3802.93,    -- Red zone x koordinatı
-        y = 4443.13,    -- Red zone y koordinatı
-        z = 4.12,    -- Red zone z koordinatı
-        radius = 92.0,   -- Red zone yarıçapı
-        visibleDistance = 300.0, -- Marker'ın görüleceği maksimum mesafe
-        color = {r = 255, g = 0, b = 0, a = 128} -- Red zone renk (RGBA)
+        x = 2349.61,    -- x koordinatı
+        y = 2557.08,    -- y koordinatı
+        z = 46.67,    -- z kordinato
+        radius = 92.0,   -- BÜYÜKLÜK
+        visibleDistance = 300.0, -- UZAKLIK
+        color = {r = 255, g = 0, b = 0, a = 128} -- RENK
     },
     -- İstediğiniz kadar red zone ekleyebilirsiniz
 }
@@ -52,8 +36,6 @@ Citizen.CreateThread(function()
                     TriggerEvent('QBCore:Notify', 'Red Zone\'dan çıktınız!', 'success')
                 end
             end
-
-            -- Draw the dome marker only if within the visible distance
             if distance < redZone.visibleDistance then
                 DrawMarker(28, redZone.x, redZone.y, redZone.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, redZone.radius, redZone.radius, redZone.radius, redZone.color.r, redZone.color.g, redZone.color.b, redZone.color.a, false, false, 2, false, nil, nil, false)
             end
@@ -64,7 +46,7 @@ end)
 Citizen.CreateThread(function()
     for _, redZone in ipairs(redZones) do
         local blip = AddBlipForRadius(redZone.x, redZone.y, redZone.z, redZone.radius)
-        SetBlipColour(blip, 1) -- 1 is red color
+        SetBlipColour(blip, 1)
         SetBlipAlpha(blip, 128)
     end
 end)
